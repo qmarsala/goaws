251ebd6ad8c56b202e248626736c537ec81596ba
Quinten Marsala <quinten.marsala@gmail.com>
2024-05-04 21:07:47 -0700

    update main cmd for db migrate

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
 	database "goaws/internal"
 )
 
 func main() {
 	db := database.ConnectDB()
-	db.AutoMigrate(database.GoAwsDbRecord{})
-	db.Model(database.GoAwsDbRecord{}).Create(&database.GoAwsDbRecord{Key: "hello", Value: "world"})
-	results := []database.GoAwsDbRecord{}
-	db.Model(database.GoAwsDbRecord{}).Find(&results)
-	fmt.Print(results)
+	db.AutoMigrate(database.Round{}, database.HandicapIndex{})
 }
