33cf52b02c8d88592a92a4c59e57bba848b7fec4
Quinten Marsala <quinten.marsala@gmail.com>
2024-05-04 17:08:02 -0700

    work on message and add result to queue

--- cmd/do-work/main.go
+++ cmd/do-work/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
 type GoAwsRequest struct {
@@ -18,14 +22,29 @@ type GoAwsResponse struct {
 }
 
 func main() {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("Couldn't load default configuration. Have you set up your AWS account?")
+	}
+	queueUrl := os.Getenv("OUTPUT_QUEUE_URL")
+	sqsClient := sqs.NewFromConfig(sdkConfig)
+
 	lambda.Start(func(ctx context.Context, event *GoAwsRequest) (*GoAwsResponse, error) {
 		if event == nil {
 			return nil, fmt.Errorf("received nil event")
 		}
 		fmt.Println("work, work.")
-		return &GoAwsResponse{
+		resp := GoAwsResponse{
 			Key:   event.Key,
 			Value: event.Value + " - I worked on this :)",
-		}, nil
+		}
+		jsonBody, _ := json.Marshal(resp)
+		body := string(jsonBody)
+		input := &sqs.SendMessageInput{
+			QueueUrl:    &queueUrl,
+			MessageBody: &body,
+		}
+		sqsClient.SendMessage(ctx, input)
+		return &resp, nil
 	})
 }
