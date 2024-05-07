4767286eae76a89251ded62238b8d236bf83fbca
Quinten Marsala <quinten.marsala@gmail.com>
2024-05-06 18:05:26 -0700

    cleanup

--- internal/database.go
+++ internal/database.go
@@ -8,6 +8,24 @@ import (
 	"gorm.io/gorm"
 )
 
+type HandicapIndex struct {
+	*gorm.Model
+	Current float32
+	Low     float32
+}
+
+type Round struct {
+	*gorm.Model
+	CourseName         string
+	CourseRating       float32
+	SlopeRating        float32
+	HolesPlayed        int
+	Score              int
+	AdjustedGrossScore int
+	Exceptional        bool
+	ThrowAway          bool
+}
+
 func ConnectDB() *gorm.DB {
 	var host = os.Getenv("POSTGRES_HOST")
 	var port = os.Getenv("POSTGRES_PORT")
