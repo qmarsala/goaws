9960d454891ac9e6759b5270b7f2a3feca07f724
Quinten Marsala <quinten.marsala@gmail.com>
2024-05-06 18:05:05 -0700

    refactor to module

--- cmd/calculate-handicap/main.go
+++ cmd/calculate-handicap/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"slices"
 
 	goaws "goaws/internal"
 
@@ -38,7 +37,7 @@ func main() {
 			fmt.Println("Error getting handicap index: ", err)
 		}
 
-		newIndex := calculateHandicapIndex(rounds)
+		newIndex := goaws.CalculateHandicapIndex(rounds)
 		fmt.Println("new index: ", newIndex)
 		if newIndex.Current == currentIndex.Current {
 			return map[string]interface{}{}, nil
@@ -48,67 +47,4 @@ func main() {
 		}
 		return map[string]interface{}{}, nil
 	})
-}
-
-func calculateHandicapIndex(rounds []goaws.Round) goaws.HandicapIndex {
-	return goaws.HandicapIndex{
-		Current: (nOutOfTwentyAverage(rounds)) - float32(getIndexAdjustment(len(rounds))),
-	}
-}
-
-func nOutOfTwentyAverage(rounds []goaws.Round) float32 {
-	diffCount := getDiffCountPerRounds(len(rounds))
-	scoreDiffs := getScoreDifferentials(rounds)
-	sum := float32(0)
-	for _, s := range scoreDiffs[:diffCount] {
-		sum += s
-	}
-	return sum / float32(diffCount)
-}
-
-func getScoreDifferentials(rounds []goaws.Round) []float32 {
-	scoreDifferentials := []float32{}
-	for _, r := range rounds {
-		scoreDifferentials = append(scoreDifferentials, calculateScoreDifferential(r))
-	}
-	slices.Sort(scoreDifferentials)
-	return scoreDifferentials
-}
-
-func calculateScoreDifferential(round goaws.Round) float32 {
-	const pcc int = 0 // todo: pcc, not totally sure what it is, though it is 0 most of the time
-	fmt.Println("calculating differential: ", round)
-	diff := (113 / round.SlopeRating) * (float32(round.AdjustedGrossScore) - round.CourseRating - float32(pcc))
-	fmt.Println(diff)
-	return diff
-}
-
-func getDiffCountPerRounds(roundCount int) int {
-	switch {
-	case roundCount < 6:
-		return 1
-	case roundCount < 9:
-		return 2
-	case roundCount < 12:
-		return 3
-	case roundCount < 15:
-		return 4
-	case roundCount < 17:
-		return 6
-	case roundCount < 19:
-		return 7
-	default:
-		return 8
-	}
-}
-
-func getIndexAdjustment(roundCount int) int {
-	switch {
-	case roundCount == 3:
-		return 2
-	case roundCount == 4 || roundCount == 6:
-		return 1
-	default:
-		return 0
-	}
 }
---
+++ internal/handicap-calculation.go
@@ -0,0 +1,69 @@
+package goaws
+
+import (
+	"fmt"
+	"slices"
+)
+
+func CalculateHandicapIndex(rounds []Round) HandicapIndex {
+	return HandicapIndex{
+		Current: (CalculateNOutOfTwentyAverage(rounds)) - float32(getIndexAdjustment(len(rounds))),
+	}
+}
+
+func CalculateNOutOfTwentyAverage(rounds []Round) float32 {
+	diffCount := getDiffCountPerRounds(len(rounds))
+	scoreDiffs := getScoreDifferentials(rounds)
+	sum := float32(0)
+	for _, s := range scoreDiffs[:diffCount] {
+		sum += s
+	}
+	return sum / float32(diffCount)
+}
+
+func getScoreDifferentials(rounds []Round) []float32 {
+	scoreDifferentials := []float32{}
+	for _, r := range rounds {
+		scoreDifferentials = append(scoreDifferentials, calculateScoreDifferential(r))
+	}
+	slices.Sort(scoreDifferentials)
+	return scoreDifferentials
+}
+
+func calculateScoreDifferential(round Round) float32 {
+	const pcc int = 0 // todo: pcc, not totally sure what it is, though it is 0 most of the time
+	fmt.Println("calculating differential: ", round)
+	diff := (113 / round.SlopeRating) * (float32(round.AdjustedGrossScore) - round.CourseRating - float32(pcc))
+	fmt.Println(diff)
+	return diff
+}
+
+func getDiffCountPerRounds(roundCount int) int {
+	switch {
+	case roundCount < 6:
+		return 1
+	case roundCount < 9:
+		return 2
+	case roundCount < 12:
+		return 3
+	case roundCount < 15:
+		return 4
+	case roundCount < 17:
+		return 6
+	case roundCount < 19:
+		return 7
+	default:
+		return 8
+	}
+}
+
+func getIndexAdjustment(roundCount int) int {
+	switch {
+	case roundCount == 3:
+		return 2
+	case roundCount == 4 || roundCount == 6:
+		return 1
+	default:
+		return 0
+	}
+}
